0dc506932979dc29358aadba7d0d6e5b1d8a8646
mshavliuk <m.shavliuk@beauit.com>
2019-05-30 21:35:28 +0300

    add ServiceStartupListenerTest

---
+++ src/Tests/EventListener/ServiceStartupListenerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mshavliuk\SignalEventsBundle\Tests\EventListener;
+
+use Exception;
+use Mshavliuk\SignalEventsBundle\EventListener\ServiceStartupListener;
+use Mshavliuk\SignalEventsBundle\Service\SignalHandlerService;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\Event;
+
+class ServiceStartupListenerTest extends TestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testListenerWillStartServiceOnce(): void
+    {
+        $containerMock = $this->createMock(ContainerInterface::class);
+        $containerMock->expects($this->once())->method('get')->with($this->equalTo(SignalHandlerService::class));
+        $listener = new ServiceStartupListener($containerMock);
+        $eventMock = $this->createMock(Event::class);
+        $listener->handleStartupEvent($eventMock);
+        $listener->handleStartupEvent($eventMock);
+        $listener->handleStartupEvent($eventMock);
+        $listener->handleStartupEvent($eventMock);
+    }
+}
