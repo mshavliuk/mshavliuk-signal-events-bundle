1cb75d5940adc8f52820df4277928044f1534a35
mshavliuk <mikhail.shavliuk@yandex.ru>
2019-06-01 21:45:58 +0300

    add ConfigurationTest and SignalEventsExtensionTest

---
+++ src/Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Mshavliuk\SignalEventsBundle\Tests\DependencyInjection;
+
+use Mshavliuk\SignalEventsBundle\DependencyInjection\Configuration;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationTest extends TestCase
+{
+    public function testConfigurationLoadDefaults(): void
+    {
+        $processor = new Processor();
+
+        $config = $processor->processConfiguration(new Configuration(), []);
+        $this->assertIsArray($config['startup_events']);
+        $this->assertNotEmpty($config['startup_events']);
+
+        $this->assertIsArray($config['handle_signals']);
+        $this->assertNotEmpty($config['handle_signals']);
+    }
+}
