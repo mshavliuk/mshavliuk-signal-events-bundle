eea75760f4198908cf67a44552752ccfa128f6df
mshavliuk <mikhail.shavliuk@yandex.ru>
2019-06-01 21:46:27 +0300

    add SignalEventsBundleTest, add more tests in SignalHandlerServiceTest

---
+++ src/Tests/SignalEventsBundleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Mshavliuk\SignalEventsBundle\Tests;
+
+
+use Mshavliuk\SignalEventsBundle\SignalEventsBundle;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class SignalEventsBundleTest extends TestCase
+{
+    public function testBundleWillCreateExtension()
+    {
+        $bundle = new SignalEventsBundle();
+        $this->assertInstanceOf(Extension::class, $bundle->getContainerExtension());
+    }
+}
